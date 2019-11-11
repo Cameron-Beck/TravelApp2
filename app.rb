e9baaf10ba68af5f103dcaed008f9d833d9a09b9
Cameron <cameron.beckit@gmail.com>
2019-11-11 17:43:03 +0000

    added a number of views

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ require_relative('controllers/city_controller.rb')
 require_relative('controllers/country_controller.rb')
 also_reload( '../models/*' )
 
-get '/' do
-  erb( :home )
+get '/home' do
+  erb(:"country/home")
 end
---
+++ controllers/country_controller.rb
@@ -0,0 +1,6 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require('pry')
+require_relative('../models/city')
+require_relative('../models/country')
+also_reload( '../models/*' )
