eba14cd349f5c112da5a22eaadd1b449422e8198
Cameron <cameron.beckit@gmail.com>
2019-11-11 17:49:28 +0000

    added paths for creating a new city and country

--- controllers/country_controller.rb
+++ controllers/country_controller.rb
@@ -4,3 +4,8 @@ require('pry')
 require_relative('../models/city')
 require_relative('../models/country')
 also_reload( '../models/*' )
+
+
+get '/home/new_country' do
+  erb(:"country/new_country")
+end
