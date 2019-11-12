51863131146c8f48b88ee914b05c3d0f5a3bdd84
Cameron <cameron.beckit@gmail.com>
2019-11-12 09:51:29 +0000

    added city description to bucketlist cities page

--- db/travels.sql
+++ db/travels.sql
@@ -12,5 +12,14 @@ CREATE TABLE cities (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
   visited BOOLEAN,
+  description TEXT,
+  country_id INT8 REFERENCES countries(id) ON DELETE CASCADE
+);
+
+CREATE TABLE cities (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  visited BOOLEAN,
+  description TEXT,
   country_id INT8 REFERENCES countries(id) ON DELETE CASCADE
 );
